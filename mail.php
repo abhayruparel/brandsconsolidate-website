35ee3b4faf06fd419b8327c182b19e41e56ed0fc
Abhay Ruparel <abhayruparel2000@gmail.com>
2022-07-03 11:26:57 +0530

    import phpmailer

---
+++ mail.php
@@ -0,0 +1,41 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+
+$mail = new PHPMailer(true);
+
+try {
+ 
+                  
+    $mail->isSMTP();                                           
+    $mail->Host       = 'smtp.gmail.com';                     
+    $mail->SMTPAuth   = true;                                   
+    $mail->Username   = 'workwithmohan@gmail.com';                    
+    $mail->Password   = 'folpsmavgthigije';                             
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            
+    $mail->Port       = 465;                                    
+
+  
+    $mail->setFrom('devninja@gmail.com', 'Dev Ninja Youtube');
+
+    $mail->addAddress('whomonugiri@gmail.com');             
+
+
+    $mail->isHTML(true);                                 
+    $mail->Subject = 'Here is the subject'.time();
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
